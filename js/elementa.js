b439eebaa4f87426cc4dd3a219210b1f6d71a8f0
Jimmy <jimmyjramirezg08@gmail.com>
2023-12-18 16:19:59 -0500

    handling-DOM

--- js/elementa.js
+++ js/elementa.js
@@ -7,13 +7,17 @@ function selectionPetPlayer() {
     let inputHipodoge = document.getElementById('hipodoge')
     let inputCapipepo = document.getElementById('capipepo')
     let inputRatigueya = document.getElementById('ratigueya')
+    let spantPetPlayer = document.getElementById('pet-player')
 
     if(inputHipodoge.checked) {
         alert('You selected Hipodoge')
+        spantPetPlayer.innerHTML = 'Hipodoge'
     } else if(inputCapipepo.checked) {
         alert('You selected Capipepo')
+        spantPetPlayer.innerHTML = 'Capipepo'
     } else if(inputRatigueya.checked) {
         alert('You selected Ratigueya')
+        spantPetPlayer.innerHTML = 'Ratigueya'
     } else {
         alert('Select a pet')
     }
