7ca0cf7f143871f863c547f1d9722cdbcf024d3b
Jimmy <jimmyjramirezg08@gmail.com>
2023-12-18 15:43:56 -0500

    functio-selection-pet-player

--- js/elementa.js
+++ js/elementa.js
@@ -1,1 +1,6 @@
-alert('Hola mundo ELEMENTA!')
+function selectionPetPlayer() {
+    alert('YOU SELECTED YOUR PET')
+}
+
+let buttonPetPlayer = document.getElementById('button-pet')
+buttonPetPlayer.addEventListener('click', selectionPetPlayer)
